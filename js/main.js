af026ad4c393140eb95a90b95e595f979e8de29c
Mathieu d'Aquin <mathieu.daquin.w@gmail.com>
2019-12-23 09:33:00 +0000

    small edits/additions

--- js/main.js
+++ js/main.js
@@ -1,49 +1,46 @@
 
 
-      // check logged in
-      // if not show dialog
+// check logged in
+// if not show dialog
+// multiple topic dropdown
 
-      // on open text - show text
-      // show right stuff - blocked when no annotation / show editing
-      // annotation event (armalian)
-      // annotation click
+// on open text - show text
+// show right stuff - blocked when no annotation / show editing
+// annotation event (armalian)
+// annotation click
+// auto complete on title
+// add stated by the author - reference to someone else's statement
+// free text about how/what is referenced
 
 var currentSelection = {};
+var usekey;
 
 $( document ).ready(function() {
-    init_panel(false);
+    fill_select('al_highlights')
+    fill_select('al_an_topic_1')
     document.addEventListener("selectionchange", function(){
 	var sel = window.getSelection();
 	var len = sel.focusOffset-sel.anchorOffset;
-	if (len>3){
+	if (len>10){
 	    currentSelection.start = sel.anchorOffset;
 	    currentSelection.lenght = len;
 	    currentSelection.text = sel.anchorNode.data.substr(currentSelection.start, currentSelection.lenght);
 	    console.log(currentSelection);
-	    init_panel(true);
+	    // remove additional topics
+	    fill_select('al_an_topic_1')
 	}
     });
 });
 
-
-function init_panel(topic_only){
-    fill_selects(topic_only);
-}
-
-function fill_selects(topic_only){
-    var st1 = '<option value="none">-- highlights --</option>'
-    var st2 = '<option value="none">-- topic --</option>'
+function fill_select(el){
+    var st1 = '<option value="none">-- concept --</option>'
     for(var i in topics){
 	var reduced = dbp_frag(topics[i])
 	var opt_string = '<option value="'+reduced+'">'+reduced.replace(/_/g,' ')+'</option>'
 	st1+=opt_string
-	st2+=opt_string
     }
-    if(!topic_only)
-	document.getElementById('al_highlights').innerHTML=st1
-    document.getElementById('al_an_topic_1').innerHTML=st2
+    document.getElementById(el).innerHTML=st1
 }
-
 
 function dbp_frag(u){
     return u.substring(u.lastIndexOf('/')+1)
