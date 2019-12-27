da9ac2b7f6f3c6728556fcb9d441f16c0178d151
Mathieu d'Aquin <mathieu.daquin.w@gmail.com>
2019-12-27 10:16:30 +0000

    initial annotation mechanics

--- js/main.js
+++ js/main.js
@@ -1,26 +1,96 @@
 var api_base = 'http://127.0.0.1:5000/'
 
-var currentSelection = {};
-var userkey;
-var loadedtext;
+var currentSelection = undefined
+var userkey
+var loadedtext
+var currentannotation
+
+var annotations = {}
 
 $( document ).ready(function() {
     fill_select('al_highlights')
     fill_select('al_an_topic_1')
     document.addEventListener("selectionchange", function(){
-	var sel = window.getSelection();
-	var len = sel.focusOffset-sel.anchorOffset;
-	if (len>10){
-	    currentSelection.start = sel.anchorOffset;
-	    currentSelection.lenght = len;
-	    currentSelection.text = sel.anchorNode.data.substr(currentSelection.start, currentSelection.lenght);
-	    console.log(currentSelection);
-	    // remove additional topics
-	    fill_select('al_an_topic_1')
+	var sel = window.getSelection()
+	var len = sel.focusOffset-sel.anchorOffset
+	if (Math.abs(len)>5){
+	    currentSelection = {}
+	    console.log(sel.toString())
+	    currentSelection.start = sel.anchorOffset
+	    currentSelection.lenght = len
+	    // currentSelection.text = sel.anchorNode.data.substr(currentSelection.start, currentSelection.lenght)
+	    currentSelection.text = sel.toString();
+	    // console.log(currentSelection);
+	    currentSelection.id = undefined
+	    init_annotation();
+	} else {
+	    // have to keep it for the change!!
+	    //currentSelection = undefined
 	}
     });
-    
+    $("#al_an_title").change(function(){update_annotation()})
 });
+
+function init_annotation(){
+    fill_select('al_an_topic_1')
+    // remove additional topics
+    $("#al_an_title").val("")
+    // clear graph
+}
+
+// base hasfunction
+// from https://gist.github.com/iperelivskiy/4110988
+var funhash = function(s) {
+    for(var i = 0, h = 0xdeadbeef; i < s.length; i++)
+        h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
+    return (h ^ h >>> 16) >>> 0;
+}
+
+function update_annotation(){
+    var aid
+    var text
+    var type
+    if (currentSelection) {
+	if (currentSelection.id){
+	    aid = currentSelection.id
+	} else {
+	    aid = userkey+"-"+loadedtext+"-"+funhash(currentSelection.text)
+	}
+    }
+    var atext = currentSelection.text
+    if ($("#ann_"+aid).length && annotations[aid]){
+	atext = annotations[aid].text
+    } else {
+	text = $("#al_text_panel").html()
+	text = text.replace(currentSelection.text, '<span id="ann_'+aid+'">'+currentSelection.text+'</span>')
+	$("#al_text_panel").html(text)	
+    }
+    type = "proposition"
+    if (!annotations[aid]) annotations[aid] = {}
+    if (document.getElementById("al_an_type_arg").checked) type = "argument"
+    annotations[aid].text = atext
+    annotations[aid].type = type
+    annotations[aid].title = $("#al_an_title").val()
+    if (type=="argument")
+	$("#ann_"+aid).css("background", "#cfc")
+    else
+	$("#ann_"+aid).css("background", "#ccf")
+    $('#ann_'+aid).removeAttr('onclick');
+    $('#ann_'+aid).attr('onClick', 'selectOnClick("'+aid+'");');
+}
+
+function selectOnClick(aid){
+    currentSelection.id = aid
+    if (annotations[aid]){
+	$("#al_an_title").val(annotations[aid].title)
+	if (annotations[aid].type=="argument")
+	    $("#al_an_type_arg").prop("checked", true)
+	else
+	    $("#al_an_type_prop").prop("checked", true)
+    } else {
+	console.log("annotation selected that does not exist...")
+    }
+}
 
 function fill_select(el){
     var st1 = '<option value="none">-- concept --</option>'
